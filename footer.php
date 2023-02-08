0e140e7f90d9d501c5fc2840670215a8199ed904
Ziyan78 <124809917+Ziyan78@users.noreply.github.com>
2023-02-08 22:59:47 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,7 @@
+	  <hr>
+
+      <footer>
+        <div class="foot_center"><p>   Online Voting System</p>
+		<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbspProgrammed by: Ziyan Khan and Heeba Ashrafi :-P</p>
+		</div>
+      </footer>
